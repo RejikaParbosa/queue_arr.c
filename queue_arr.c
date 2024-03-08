4c0a252ae0732fb094fcd99cf1371dd2b3b81023
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-03-08 12:28:50 +0530

    queue_arr.c
    implementation of queue using array

--- queue_arr.c
+++ queue_arr.c
@@ -1,6 +1,7 @@
+// Implementation of Queue using array
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 5
+#define MAX 3
 
 int queue_arr[MAX];
 int rear=-1;
@@ -13,14 +14,14 @@ insert()
 		printf("Queue Overflow\n");
 	else
 	{
-		if(front==-1) /*If queue is initially empty*/
+		if(front==-1)
 			front=0;
-		printf("Input the element for adding in queue:");
+		printf(" Input the element for adding in queue: ");
 		scanf("%d",&added_item);
 		rear=rear+1;
 		queue_arr[rear]=added_item;
 	}
-}/*End of insert*/
+}
 
 del()
 {
@@ -31,11 +32,11 @@ del()
 	}
 	else
 	{
-		printf("Element deleted from queue is:%d\n",queue_arr[front]);
+		printf("Element deleted from queue is: %d\n",queue_arr[front]);
 		front=front+1;
 	}
 }
-/*End of delete*/
+
 
 display()
 {
@@ -46,23 +47,25 @@ display()
 	{
 		printf("Queue is:\n");
 		for(i=front;i<=rear;i++)
-			printf("%d",queue_arr[i]);
+			printf("\n %d",queue_arr[i]);
 		printf("\n");
 	}
-}/*End of display()*/
+}
 
 main()
 {
 	int choice;
 	while(1)
 	{
+		printf("\n");
+		printf("\tMENU\n");
 		printf("1. Insert\n");
 		printf("2. Delete\n");
 		printf("3. Display\n");
-		printf("4. Quit\n");
-		printf("Enter your choice:");
+		printf("4. Exit\n");
+		printf("\n Enter your choice: ");
 		scanf("%d",&choice);
-		
+
 		switch(choice)
 		{
 			case 1:
@@ -78,6 +81,7 @@ main()
 				exit(1);
 			default:
 				printf("Wrong choice\n");
-		}/*End of switch*/
-	}/*End of while*/
-}/*End of main*/
+		}
+	}
+}
+
